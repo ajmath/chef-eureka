71ccff3a31f8265dcb22fd5edcba6f25b130cf11
Andrew Matheny <andrew.j.matheny@gmail.com>
2014-09-05 09:34:32 -0400

    Add name to metadata.rb
    With the current version of berkshelf, vendoring fails due to a hard requirement that cookbooks have explicitly defined a name in metadata.rb.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license          "Apache 2.0"
 description      "Installs/Configures eureka"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+name             "chef-eureka"
 
 %w{ tomcat }.each do |cb|
   depends cb
